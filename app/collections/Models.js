7755f785d66cc65e82c6c75111320ab6e66c858e
Michael Prather <mprather@skynet.shintech.ninja>
2018-02-28 03:37:09 +0000

    fixed url

--- app/collections/Models.js
+++ app/collections/Models.js
@@ -2,7 +2,7 @@ import Model from '../models/Model'
 
 const Models = Backbone.Collection.extend({
   model: Model,
-  url: 'http://skynet.shintech.ninja:8000/api/models',
+  url: '/api/models',
   parse: function (data) {
     return data['body'].results
   }
