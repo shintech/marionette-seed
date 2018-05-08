df49f3c2026f6a532a4c72adfdec4ddee29c09a3
Michael Prather <mprather1@gmail.com>
2018-05-08 15:50:42 +0000

    updated install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,17 @@
 
 if [ -d "node_modules" ] || [ -d "build" ]; then
   echo "Removing existing files..."
-  rm -rv node_modules build --force
+  rm -rv node_modules dist --force
 fi
 
 echo "Creating file directories..." && \
-mkdir build && \
+mkdir dist && \
 
 printf "\nInstalling packages...\n" && \
 yarn install && \
 
 printf "\nBuilding in progress...\nPlease wait...\n\n" && \
 npm run -s build && \
+webpack && \
 
 printf "All done...\n"
