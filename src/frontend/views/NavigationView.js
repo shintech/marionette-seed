28e376159683205c1bcfe4371c2ace316c8d0c84
Michael Prather <mprather1@gmail.com>
2018-05-10 03:28:06 +0000

    added navbar to rootview

--- src/frontend/views/NavigationView.js
+++
@@ -1,7 +0,0 @@
-const NavigationView = Backbone.Marionette.View.extend({
-  className: 'header',
-  tagName: 'nav',
-  template: require('../templates/navigation-view-template.html')
-})
-
-export default NavigationView
--- src/frontend/views/RootView.js
+++ src/frontend/views/RootView.js
@@ -1,23 +1,13 @@
-import NavigationView from './NavigationView'
-
 const RootView = Backbone.Marionette.View.extend({
   className: 'root',
   template: require('../templates/root-view-template.html'),
   regions: {
-    header: {
-      el: '.header',
-      replaceElement: true
-    },
     content: {
       el: '.content'
     },
     modal: {
       el: '.root-modal'
     }
-  },
-
-  onRender: function () {
-    this.showChildView('header', new NavigationView())
   }
 })
 
